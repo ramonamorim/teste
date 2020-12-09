023f278c60766d3edfa8a7ca5765180cdfb6591a
ramonamorim <amorimramon@Outlook.com>
2020-12-09 11:14:06 -0300

    initial

---
+++ teste-spring-sqs/src/main/java/com/example/testespringsqs/TesteSpringSqsApplication.java
@@ -0,0 +1,13 @@
+package com.example.testespringsqs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteSpringSqsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TesteSpringSqsApplication.class, args);
+    }
+
+}
---
+++ teste-spring-sqs/src/test/java/com/example/testespringsqs/TesteSpringSqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.testespringsqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteSpringSqsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
